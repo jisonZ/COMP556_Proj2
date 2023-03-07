d86cf0e75f74cb7ef2e22685e0d7984d72d7d3cb
jisonZ <haochenz@umich.edu>
2023-03-06 23:03:21 -0600

    finished sendfile, utils

--- sendfile.c
+++ sendfile.c
@@ -8,12 +8,9 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <sys/time.h>
+#include "utils.c"
 
-#define MAX_BUFF 1000
-#define PKT_SIZE 1024
-#define WINDOW_LEN 5
-#define TIME_OUT 10
-struct packet 
+struct packet
 {
     int seqNum;
     int buffPos;
@@ -22,17 +19,21 @@ struct packet
     timeval sendTime;
 };
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
 
-    char* serverAddr = NULL;
+    char *serverAddr = NULL;
     unsigned short serverPort;
-    char* fname = NULL;
+    char *fname = NULL;
 
-    if (argc == 3) {
+    if (argc == 3)
+    {
         serverAddr = argv[0];
         serverPort = atoi(argv[1]);
         fname = argv[2];
-    } else {
+    }
+    else
+    {
         perror("usage: ./sendfile <server address> <server port> <file name>\n");
         abort();
     }
@@ -51,7 +52,7 @@ int main(int argc, char** argv) {
         server_addr = (unsigned int)((struct sockaddr_in *)(getaddrinfo_result->ai_addr))->sin_addr.s_addr;
         freeaddrinfo(getaddrinfo_result);
     }
-    
+
     /* ----- initialize and connect socket ------ */
     int sock;
     if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
@@ -73,24 +74,33 @@ int main(int argc, char** argv) {
     printf("Client is Connected\n");
 
     /* ----- create buffer ----- */
-    char* buffer = (char *)malloc(MAX_BUFF*PKT_SIZE);
-    if (!buffer) {
+    char *filebuffer = (char *)malloc(MAX_BUFF * PKT_SIZE);
+    if (!buffer)
+    {
         perror("failed to allocate file buffer\n");
         abort();
     }
     int RECV_LEN = 12;
-    char* recvbuffer = (char *)malloc(RECV_LEN);
-    if (!recvbuffer) {
+    char *recvbuffer = (char *)malloc(RECV_LEN);
+    if (!recvbuffer)
+    {
         perror("failed to allocate recv buffer\n");
         abort();
     }
-    int SEND_LEN = PKT_SIZE+16;
-    char* sendbuffer = (char *)malloc(SEND_LEN);
-    if (!sendbuffer) {
+    int SEND_LEN = PKT_SIZE + 16;
+    char *sendbuffer = (char *)malloc(SEND_LEN);
+    if (!sendbuffer)
+    {
         perror("failed to allocate send buffer\n");
         abort();
     }
 
+    char *msgbuffer = (char *)malloc(PKT_SIZE);
+    if (!msgbuffer)
+    {
+        perror("failed to allocate msg buffer\n");
+        abort();
+    }
     /* ----- send file ----- */
     fd_set read_set;
     fd_set write_set;
@@ -102,7 +112,7 @@ int main(int argc, char** argv) {
 
     int select_retval;
 
-    FILE* fp;
+    FILE *fp;
     fp = fopen(fname, "r");
 
     int seq_num = 0;
@@ -112,21 +122,24 @@ int main(int argc, char** argv) {
     while (!EOF)
     {
         /* read from file to buffer */
-        int bufferSize = fread(buffer, 1, MAX_BUFF*PKT_SIZE, fp);
-        if (bufferSize < MAX_BUFF*PKT_SIZE) {
+        int bufferSize = fread(filebuffer, 1, MAX_BUFF * PKT_SIZE, fp);
+        if (bufferSize < MAX_BUFF * PKT_SIZE)
+        {
             EOF = true;
         }
         /* implement bufferSize/PKT_SIZE, EOF signal*/
-        bufferSize /= PKT_SIZE;
+        int bufferCount = bufferSize /= PKT_SIZE;
 
         /* initialize window */
         int buff_pos = 0;
-        packet* window = (packet*) malloc(WINDOW_LEN * sizeof(packet));
-        if (window == NULL) {
+        packet *window = (packet *)malloc(WINDOW_LEN * sizeof(packet));
+        if (window == NULL)
+        {
             perror("fail to allocate window buffer\n");
             abort();
         }
-        for (int i = 0; i < WINDOW_LEN; ++i) {
+        for (int i = 0; i < WINDOW_LEN; ++i)
+        {
             struct packet p;
             p.seqNum = seq_num;
             p.buffPos = buff_pos;
@@ -136,9 +149,9 @@ int main(int argc, char** argv) {
             seq_num++;
             buff_pos++;
         }
- 
-         /* send buffer */
-        while (1) 
+
+        /* send buffer */
+        while (1)
         {
             /* we only need to listen from the sockets */
             FD_ZERO(&read_set);
@@ -149,70 +162,116 @@ int main(int argc, char** argv) {
             max = sock;
 
             /* select() for ACK */
-            select_retval = select(max+1, &read_set, NULL, NULL, &timeout);
-            if (select_retval < 0) {
+            select_retval = select(max + 1, &read_set, NULL, NULL, &timeout);
+            if (select_retval < 0)
+            {
                 perror("select failed");
                 abort();
             }
-            if (select_retval > 0) {
-                if (FD_ISSET(sock, &read_set)) {
+            if (select_retval > 0)
+            {
+                if (FD_ISSET(sock, &read_set))
+                {
                     /* take ACK */
-                    int recvLen = recv(sock, recvbuffer, RECV_LEN, 0);
+                    int recvLen = recv(sock, recvbuffer, RECV_LEN, 0); // Ack
+                    int ack_num;
+                    int ack_status = decode_ACK(recvbuffer, recvLen, &ack_num);
 
+                    int first_window_seq = window[0].seqNum;
+                    int last_window_seq = window[WINDOW_LEN - 1].seqNum;
+                    int window_pos = ack_num - first_window_seq;
+                    if (ack_num >= first_window_seq || ack_num <= last_window_seq)
+                    {
+                        if (ack_status == -2)
+                        {
+                            window[window_pos].send = false;
+                        }
+                        else if (ack_status == 0)
+                        {
+                            window[ack_num - first_window_seq].ack = true;
+                        }
+                    }
+                    // if (FD_ISSET(sock, &write_set)) {
+                    //     /* continue unsuccessful write */
+                    //     /* might be too complicated to implement,
+                    //     alternative is to wait till all of the message is sent */
+                    // }
                 }
-                // if (FD_ISSET(sock, &write_set)) {
-                //     /* continue unsuccessful write */
-                //     /* might be too complicated to implement,
-                //     alternative is to wait till all of the message is sent */
-                // }
-            }
 
-            /* detect window shift */
-            int shift = 0;
-            for (int i = 0; i < WINDOW_LEN; ++i) {
-                if (window[i].ack) {
-                    shift++;
-                } else {
+                /* detect window shift */
+                int shift = 0;
+                for (int i = 0; i < WINDOW_LEN; ++i)
+                {
+                    if (window[i].ack)
+                    {
+                        shift++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (shift != 0)
+                {
+                    /* move window */
+                    for (int i = 0; i < WINDOW_LEN - shift; ++i)
+                    {
+                        window[i] = window[i + shift];
+                    }
+
+                    /* reset rest of window */
+                    for (int i = WINDOW_LEN - shift; i < WINDOW_LEN; ++i)
+                    {
+                        struct packet p;
+                        p.seqNum = seq_num;
+                        p.buffPos = buff_pos;
+                        p.ack = false;
+                        p.send = false;
+                        window[i] = p;
+                        seq_num++;
+                        buff_pos++;
+                    }
+                }
+
+                /* detect if all item been sent*/
+                if (window[0].buffPos > bufferCount)
+                {
                     break;
                 }
-            }
-            if (shift != 0) {
-                /* move window */
-                for (int i = 0; i < WINDOW_LEN-shift; ++i) {
-                    window[i] = window[i+shift];
-                }
 
-                /* reset rest of window */
-                for (int i = WINDOW_LEN-shift; i < WINDOW_LEN; ++i) {
-                    struct packet p;
-                    p.seqNum = seq_num;
-                    p.buffPos = buff_pos;
-                    p.ack = false;
-                    p.send = false;
-                    window[i] = p;
-                    seq_num++;
-                    buff_pos++;
-                }
-            }
+                /* send frames */
+                for (int i = 0; i < WINDOW_LEN; ++i)
+                {
+                    struct timeval current_time;
+                    gettimeofday(&current_time, NULL);
 
-            /* detect if all item been sent*/
-            if (window[0].buffPos > bufferSize) {
-                break;
-            }
-
-            /* send frames */
-            for (int i = 0; i < WINDOW_LEN; ++i) {
-                struct timeval current_time;
-                gettimeofday(&current_time, NULL);
-
-                if (!window[i].ack || current_time - window[i].sendTime > TIME_OUT) {
-                    /* fully send message */
-                    if (EOF && window[i].buffPos == bufferSize - 1) {
-                        /* send EOF in packet */
+                    if (!window[i].send ||
+                        (!window[i].ack && current_time - window[i].sendTime > TIME_OUT))
+                    {
+                        /* fully send message */
+                        // send(sock, window[i].buffPos, 0);
+                        msgbuffer = filebuffer + window[i].buffPos * PKT_SIZE;
+                        int msgSize;
+                        if (bufferCount * PKT_SIZE > bufferSize)
+                        {
+                            msgSize = bufferSize - (bufferCount - 1) * PKTSIZE;
+                        }
+                        else
+                        {
+                            msgSize = PKTSIZE;
+                        }
+                        if (EOF && window[i].buffPos == bufferCount - 1)
+                        {
+                            /* send EOF in packet */
+                            encode_send(windows[i].seqNum, sendbuffer, msgbuffer, 1, msgSize);
+                        }
+                        else
+                        {
+                            encode_send(windows[i].seqNum, sendbuffer, msgbuffer, 0, msgSize);
+                        }
                     }
                 }
             }
         }
     }
-
 }
--- utils.c
+++ utils.c
@@ -2,51 +2,107 @@
 #include <stdio.h>
 #include <arpa/inet.h>
 
-unsigned short checksum(char *frame, int count) {
+#define MAX_BUFF 1000
+#define PKT_SIZE 1024
+#define WINDOW_LEN 5
+#define TIME_OUT 10
+
+unsigned short checksum(char *frame, int count)
+{
     unsigned long sum = 0;
-    while (count--) {
+    while (count--)
+    {
         sum += *frame++;
-        if (sum & 0xFFFF0000) {
+        if (sum & 0xFFFF0000)
+        {
             sum &= 0xFFFF;
-            sum++; 
+            sum++;
         }
     }
     return (sum & 0xFFFF);
 }
 
-void encode_ACK() {
-    /* 
+void encode_ACK(int seqNum, int error, char *ack_buff)
+{
+    /*
     int seqNum;
     int error;
-    int eof;
     int checkSum;
     */
+
+    *((int *)ack_buff) = htonl(seqNum);
+    *((int *)ack_buff + 1) = htonl(error);
+    int checkSum = checksum(ack_buff, 8);
+    *((int *)ack_buff + 2) = htonl(checkSum);
+    return;
 }
 
-int decode_ACK(char* buf, int len) {
-    /* TODO: is detecting length correct?*/
-    /* Reasoning: UDP itself is used for tranfering long msgs, 
+int decode_ACK(char *buf, int len, *int seqNum)
+{
+    /* Reasoning: UDP itself is used for tranfering long msgs,
     therefore we dont care */
-    /* return -1: not enough byte*/
-    if (len != 12) return -1;
-    int seqNum = ntohl(*(int*)buf);
-    int error = ntohl(*((int*)buf+1));
-    int eof = ntohl(*((int*)buf+2));
-    int checkSum =  ;
-    if ()
+    /* return -1: not enough byte recived or error*/
+    if (len != 12)
+        return -1;
+    *seqNum = ntohl(*(int *)buf);
+    int error = ntohl(*((int *)buf + 1));
+    int checkSum = ntohl(*((int *)buf + 2));
+
+    if (checksum(buf, 8) != checkSum)
+    {
+        return -1;
+    }
+    else if (error == 1)
+    {
+        return -2;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
-void encode_send() {
+void encode_send(int seqNum, char *send_buff, char *msg, int EOF, int size)
+{
     /*
     int size;
     int seqNum;
-    char* msg;
     int EOF;
+    char* msg;
     int checkSum;
     */
+
+    *((int *)send_buff) = htonl(size + 16);
+    *((int *)send_buff + 1) = htonl(seqNum);
+    *((int *)send_buff + 2) = htonl(EOF);
+    *((char *)((int *)send_buff + 3)) = *msg;
+    int checkSum = checksum(send_buff, 12 + size);
+    *((int *)send_buff + size + 12) = htonl(checkSum);
+    return;
 }
 
-void decode_send() {
+int decode_send(char *buf, int len, int *seqNum, char *msg, int *msgSize)
+{
 
+    int size = ntol(*(int *)buf);
+    // Check Size is  correct
+    if (size != len)
+        return -1;
+
+    int checkSum = ntol(buf + size - 4);
+    // CheckSum Exam
+    if (checkSum != checksum(buf, size - 4))
+        return -1;
+
+    // Copy MSG and its length
+    *msg = *(buf + 12);
+    *msgSize = size - 16;
+
+    // set seqNum
+    int seqnum = ntol(*((int *)buf + 1));
+    *seqNum = seqnum;
+
+    // Return EOF
+    int EOF = ntol(*((int *)buf + 2));
+    return EOF;
 }
-
